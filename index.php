1dce311f4a70f62588122e713a7a81e35d70efc4
AlexisHenry <alexis.henry150357@gmail.com>
2022-12-15 17:21:57 +0100

    fix: upload file who doesn't exist to fix bugs

--- index.php
+++ index.php
@@ -32,77 +32,14 @@ function download(string $category, string $file, string $domain) {
 }
 
 /**
- * Custom JSON response
- * 
- * @param array $response
- * @param int $response_code
- * @param string $domain
- */
-function JsonResponse(array $response, int $response_code, string $domain) {
-	header('Content-Type: application/json; charset=utf-8');
-	echo json_encode(
-		array_merge(
-			[
-				"url" => $domain . $_SERVER["REQUEST_URI"],
-				"response_code" => $response_code,
-				"response_time" => date("Y-m-d H:i:s")
-			],
-			$response
-		)
-	);
-	die();
-}
-
-/**
  * @var string $request
- */
-$request = $_SERVER["REQUEST_URI"];
-
-/**
- * Check if the no category is specified
- */
-if ($request === "/") {
-	JsonResponse([
-		"status" => "The request can't be processed. Please specify a category.",
-		"message" => "Please specify a category in the URL. For example: $domain/{files|images|videos}.",
-		"category_specified" => null,
-		"file_specified" => null
-	], 400, $domain);
-}
-
-/**
  * @var string $category
  * @var string $file
  */
+$request = $_SERVER["REQUEST_URI"];
 $category = strtolower(explode("/", $request)[2]);
 $file = $_GET["file"] ?? null;
 
-/**
- * Check if the file name is specified
- */
-if (!$file) {
-	JsonResponse([
-		"status" => "The request can't be processed. Please specify a $category name.",
-		"message" => "Please specify a file name in the URL. For example: $domain/$category/file-name.ext.",
-		"category_specified" => $category,
-		"file_specified" => null
-	], 400, $domain);
+if (file_exists(__DIR__ . "/public/$category/$file")) {
+	download($category, $file, $domain);
 }
-
-/**
- * Check if the file exists
- */
-$filename = __DIR__ ."/public/$category/$file";
-if (!file_exists($filename)) {
-	JsonResponse([
-		"status" => "The requested file doesn't exist.",
-		"message" => "The requested file doesn't exist. Please check the URL parameters specified and retry.",
-		"category_specified" => $category,
-		"file_specified" => $file
-	], 404, $domain);
-}
-
-/**
- * Download the file
- */
-download($category, $file, $domain);

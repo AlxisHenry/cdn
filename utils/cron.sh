346a5305137bc524f175a4d1be716167227fc41e
AlexisHenry <alexis.henry150357@gmail.com>
2023-01-23 09:42:40 +0100

    feat(cron): configure save cron

---
+++ utils/cron.sh
@@ -0,0 +1,24 @@
+# This script is made to set a cron task used to save your shared files
+# To configure the cron, change the variable below:
+
+source utils/tools.sh;
+
+# Duration between running crons
+duration="5 10 * * *";
+
+# Path to shared folder
+shared="/var/www/cdn.alexishenry.eu/public/shared";
+
+# Save folder path
+saved="/home/alexis/save_my_files";
+
+function crontab() {
+	if [[ ! -d ${saved} ]]; then
+		throw "Save folder specified [ ${saved} ] does not exist" true;
+	elif [ "$EUID" -ne 0 ]; then 
+		throw "Please, run this script as root" true;
+	fi
+	echo "${duration} cp -r ${shared} ${saved}/cdn_save_$(date +%s)" > /etc/cron.hourly/cdn-autosave
+}
+
+crontab;
